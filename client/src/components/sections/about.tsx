6d75aae21deb86fe4a91192f2f756e1873c0c7da
sid111rawal <sid111rawal@gmail.com>
2025-05-03 06:20:49 +0000

    Fix: Corrected image paths in About section, services page, and seed file

--- client/src/components/sections/about.tsx
+++ client/src/components/sections/about.tsx
@@ -65,7 +65,7 @@ export default function About() {
                 
                 <div className="relative z-10">
                   <img 
-                    src="/src/images/my-new-profile.png"
+                    src="/images/my-new-profile.png"
                     alt="Sid Rawal - Web Developer" 
                     className="w-full h-auto rounded-lg mb-6"
                   />
